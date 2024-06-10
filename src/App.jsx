03bd9d95119e0929a73403fe4dc99b56f617a0bb
VikasDewangan <dewanganvikas192@gmail.com>
2024-06-10 09:40:27 +0530

    adding the files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+ 
+
+function App() {
+ 
+  let handleClick = ()=>{
+    console.log("button is Clicked");
+  }
+
+  return (
+    <>
+       <h1>Mini-Project</h1>
+
+    </>
+  )
+}
+
+export default App
